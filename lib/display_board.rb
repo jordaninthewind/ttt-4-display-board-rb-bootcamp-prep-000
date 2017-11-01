e497c06824a25a8a6ade224aa6cba9aebf20fa93
jordaninthewind <jordan.kline@gmail.com>
2017-11-01 00:57:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,17 +2,17 @@
 # out the current state.
 # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-row = []
-row[0] = [" #{board[0]} ", "|", " #{board[1]} ", "|", " #{board[2]} " ]
-row[1] = ["-----------"]
-row[2] = [" #{board[3]} " , "|", " #{board[4]} ", "|", " #{board[5]} "]
-row[3] = ["-----------"]
-row[4] = [" #{board[6]} ", "|", " #{board[7]} ", "|", " #{board[8]} "]
+# row = []
+# row[0] = [" #{board[0]} ", "|", " #{board[1]} ", "|", " #{board[2]} " ]
+# row[1] = ["-----------"]
+# row[2] = [" #{board[3]} " , "|", " #{board[4]} ", "|", " #{board[5]} "]
+# row[3] = ["-----------"]
+# row[4] = [" #{board[6]} ", "|", " #{board[7]} ", "|", " #{board[8]} "]
 
 def display_board(board)
-  puts row[0]
-  puts row[1]
-  puts row[2]
-  puts row[3]
-  puts row[4]
+  print row[0] = [" #{board[0]} ", "|", " #{board[1]} ", "|", " #{board[2]} " ]
+  print row[1] = ["-----------"]
+  print row[2] = [" #{board[3]} " , "|", " #{board[4]} ", "|", " #{board[5]} "]
+  print row[3] = ["-----------"]
+  print row[4] = [" #{board[6]} ", "|", " #{board[7]} ", "|", " #{board[8]} "]
 end

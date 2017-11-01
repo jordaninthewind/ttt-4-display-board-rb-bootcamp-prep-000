6d24aa33a6ea710ff5318be67ab23bdc336c3767
jordaninthewind <jordan.kline@gmail.com>
2017-11-01 00:50:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,4 +10,9 @@ row[3] = ["-----------"]
 row[4] = [" #{board[6]} ", "|", " #{board[7]} ", "|", " #{board[8]} "]
 
 def display_board(board)
-  puts 
+  puts row[0]
+  puts row[1]
+  puts row[2]
+  puts row[3]
+  puts row[4]
+end

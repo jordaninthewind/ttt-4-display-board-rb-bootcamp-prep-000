ce7b5cbac6c5de421ee2255118d15a5e521fc68b
jordaninthewind <jordan.kline@gmail.com>
2017-11-01 01:15:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,16 +3,16 @@
 # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 row = []
-# row[0] = [" #{board[0]} ", "|", " #{board[1]} ", "|", " #{board[2]} " ]
-# row[1] = ["-----------"]
-# row[2] = [" #{board[3]} " , "|", " #{board[4]} ", "|", " #{board[5]} "]
-# row[3] = ["-----------"]
-# row[4] = [" #{board[6]} ", "|", " #{board[7]} ", "|", " #{board[8]} "]
+row[0] = " #{board[0]} | #{board[1]} | #{board[2]} "
+row[1] = "-----------"
+row[2] = " #{board[3]} | #{board[4]} | #{board[5]} "
+row[3] = "-----------"
+row[4] = " #{board[6]} | #{board[7]} | #{board[8]} "
 
 def display_board(board)
-  print row[0] = [" #{board[0]} ", "|", " #{board[1]} ", "|", " #{board[2]} " ]
-  print row[1] = ["-----------"]
-  print row[2] = [" #{board[3]} " , "|", " #{board[4]} ", "|", " #{board[5]} "]
-  print row[3] = ["-----------"]
-  print row[4] = [" #{board[6]} ", "|", " #{board[7]} ", "|", " #{board[8]} "]
+  puts row[0]
+  puts row[1]
+  puts row[2]
+  puts row[3]
+  puts row[4]
 end
